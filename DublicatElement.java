e6bad13cd245cc261d0cfb2f2c2ecfe71675c94c
Saurabh kumar <saurabhtullu@gmail.com>
2023-12-22 16:36:53 +0530

    Dublicate element find using hashmap and without using collection

--- DublicatElement.java
+++ DublicatElement.java
@@ -1,9 +1,13 @@
 package Java_interview;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DublicatElement {
     public static void main(String[] args) {
         String lang[]={"java","python","java","c","php"};
-        
+
+        //1. using for loop wast soluation
         for(int i=0;i<lang.length;i++){
             for(int j=i+1;j<lang.length;j++){
                 if(lang[i].equals(lang[j])) {
@@ -12,6 +16,18 @@ public class DublicatElement {
                 }
             }
         }
+
+        System.out.println("**********************");
+        //2.  Using HashSet java collection : it store unique value
+        Set<String> store= new HashSet<String>();
+        for(String name:lang){
+            if(store.add(name)==false){
+                System.out.println(name);
+            }
+        
+        }
+
+
     }
     
 }
